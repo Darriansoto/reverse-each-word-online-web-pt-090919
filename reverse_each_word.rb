39a5b90f5f5310d4a0e736f8b8fab2b59e3f7abe
Darrian Soto <ddog350@gmail.com>
2019-09-25 01:05:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,8 @@
 def reverse_each_word(sentence)
-  new_array = sentence.split(" ")
-  
+  array = sentence.split(" ")
+  new_array = []
+  array.each do |word|
+    new_array << word.reversed
   end
   new_array
 end
