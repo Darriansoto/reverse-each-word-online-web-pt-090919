107822e84f84e9cb8ff38c6de6bd8aaadddb2655
Darrian Soto <ddog350@gmail.com>
2019-09-25 01:08:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,11 +7,11 @@ def reverse_each_word(sentence)
   new_array.join(" ")
 end
 
-
-# Hello there, and how are you?
-
-# olleH ,ereht dna woh era ?uoy
-
-# Hi again, just making sure it's reversed!
-
-# iH ,niaga tsuj gnikam erus s'ti !desrever
+def reverse_each_word(sentence)
+  array = sentence.split(" ")
+  new_array = []
+  array.collect do |word|
+    new_array << word.reverse
+  end
+  new_array.join(" ")
+end
