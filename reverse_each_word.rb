08a9daf194e82d44259244180130988ebe5b48e8
Darrian Soto <ddog350@gmail.com>
2019-09-25 01:06:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
 def reverse_each_word(sentence)
   array = sentence.split(" ")
-  new_array = []
+  new_array = ""
   array.each do |word|
     new_array << word.reverse
   end
-  new_array
+  new_array.join(" ")
 end
 
 
